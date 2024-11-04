73381a7270779bbc066a922e9421ef5ffd84ccd2
PSTL-02 <tbnrneelplayz@gmail.com>
2024-11-05 11:58:07 +1300

    photo change

--- components/Common/Intro/index.jsx
+++ components/Common/Intro/index.jsx
@@ -8,7 +8,7 @@ import Tools from './Tools';
 import Skills from './Skills';
 import Image from 'next/image';
 import { NAME, DESIGNATION, SOCIAL_LINKS } from '../../../constants/constants';
-import Neel from '../../../public/images/neel.JPG';
+import Neel from '../../../public/images/neel.png';
 
 const Intro = () => {
   return (
